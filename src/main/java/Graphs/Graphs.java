48b415899b33e76f99a4a0156be8e586cc0b8cfb
Jacek333 <jacekswitalski@poczta.fm>
2017-08-26 13:52:40 +0200

    + Graphs

--- src/main/java/Graphs/Graphs.java
+++ src/main/java/Graphs/Graphs.java
@@ -3,9 +3,10 @@ package Graphs;
 public class Graphs {
     public static void main(String[] args) {
 
-        generateGnk(4,3);
+//        generateGnk(4,3);
 //        generateGNP(4,0.5);
-        transformEtoA(4,3);
+//        transformEtoA(4,3);
+        transformAtoE();
     }
 
     public static void generateGNP(int n, double p) {
@@ -17,6 +18,12 @@ public class Graphs {
     public static void generateGnk(int n, int k) {
         Edge[] result = GraphGnk.generateGnk(n, k);
         PrintStructures.printE(result);
+    }
+    public static void transformAtoE(int n, double p){
+        boolean[][] a = GraphGnp.generateGnp(n,p);
+        PrintStructures.printA(a);
+        Edge[] e = Transforms.transformAtoE(a,n);
+        PrintStructures.printE(e);
     }
     public static void transformEtoA(int n, int k) {
         Edge[] graphGnk = GraphGnk.generateGnk(n, k);
